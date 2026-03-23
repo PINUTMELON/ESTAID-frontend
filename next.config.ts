9a5f4fc990e10e2851463e60c867375173534af4
nacong <baskinnabeens@gmail.com>
2026-03-23 22:17:52 +0900

    fix: 로그인 오류 해결

--- next.config.ts
+++ next.config.ts
@@ -10,14 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
